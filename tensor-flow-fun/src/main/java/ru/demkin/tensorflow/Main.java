614c4d05e8148ecbee8650dedcdf17d4fc7bebcd
Eugene <Evgeny.Demkin@moex.com>
2017-08-28 10:25:18 +0300

    added skeleton project

---
+++ tensor-flow-fun/src/main/java/ru/demkin/tensorflow/Main.java
@@ -0,0 +1,12 @@
+package ru.demkin.tensorflow;
+
+/**
+ * @author evgeny.demkin@moex.com
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+
+}
